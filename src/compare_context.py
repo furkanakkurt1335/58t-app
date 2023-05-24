d132ced954d2674d41b7aca49da2354735a8d7bf
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2023-05-24 12:03:19 +0300

    update script: increasing context size works

--- src/compare_context.py
+++ src/compare_context.py
@@ -7,9 +7,8 @@ config = AutoConfig.from_pretrained('bert-base-uncased', output_hidden_states=Tr
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', config=config)
 model = BertModel.from_pretrained('bert-base-uncased', config=config)
 
-sentences = ['I went to the bank to deposit my money.', 'The river bank was full of dead fishes.', 'The bank was closed since it was Sunday.', 'I went to the bank to withdraw my money.', 'I went to the bank to withdraw my money.']
-sentences = ['The river bank was full of dead fishes.', 'The bank was closed since it was Sunday.', 'I went to the bank to deposit my money.', 'I went to the bank to withdraw my money.']
-words = ['bank', 'bank', 'bank', 'bank']
+sentences = ['The river bank was full of dead fishes. I was afraid to swim there. The putrid smell emanating from the dead fish was overwhelming, and it made me feel sick to my stomach. I wondered what could have caused such a catastrophic event, as I had never seen so many dead fish in one place before. Perhaps there had been a chemical spill or some other environmental disaster that had contaminated the river. Whatever the cause, it was clear that the water was not safe to swim in, and I decided to find another place to cool off on this hot summer day.', 'I went to the bank to withdraw my money. The bank was closed since it was Sunday. As I approached the bank, I noticed that the usual hustle and bustle of people coming and going was notably absent. It was then that I realized that it was Sunday, and the bank was closed. I had completely forgotten about the limited hours of operation on weekends, and the disappointment was palpable. I needed to withdraw my money urgently to pay for some bills, and I didn\'t know what to do next. I made a mental note to check the bank\'s hours of operation in the future to avoid similar situations and decided to explore other options, such as online banking or using an ATM.']
+word = 'bank'
 repr_l = []
 # Tokenize the input sequence
 for i, sentence in enumerate(sentences):
@@ -21,18 +20,19 @@ for i, sentence in enumerate(sentences):
     outputs = model(input_ids=input_ids, attention_mask=attention_mask)
 
     # Retrieve the contextualized representations from the model's hidden states (the last 4 layers) and average them
-    token_index = tokens.index(words[i])
-    token_representation = outputs.hidden_states[-1][0][token_index]
-    for j in range(2, 5):
-        token_representation += outputs.hidden_states[-j][0][token_index]
-    token_representation /= 4
-    repr_l.append({'sentence': sentence, 'token': tokens[token_index], 'representation': token_representation.detach().numpy()})
+    tokens_temp = tokens.copy()
+    for order in range(tokens.count(word)):
+        token_index = tokens_temp.index(word)
+        token_representations = [i[0][token_index] for i in outputs.hidden_states[-4:]]
+        concat_repr = torch.cat(token_representations, dim=0)
+        repr_l.append({'sentence': ' '.join(tokens_temp), 'representation': concat_repr.detach().numpy()})
+        tokens_temp[token_index] = 'X'
 
 out_l = []
 for i, repr1 in enumerate(repr_l):
     for j, repr2 in enumerate(repr_l[i+1:]):
         dist = cosine(repr1['representation'], repr2['representation'])
-        out_l.append({'sent1': repr1['sentence'], 'sent2': repr2['sentence'], 'dist': dist, 'order': f'{i+1}-{j+i+2}'})
-out_l.sort(key=lambda x: x['order'])
-for out in out_l:
-    print('{order} {dist:.4f}: "{sent1}" and "{sent2}"'.format(sent1=out['sent1'], sent2=out['sent2'], dist=out['dist'], order=out['order']))
+        print('Sentence 1:', repr1['sentence'])
+        print('Sentence 2:', repr2['sentence'])
+        print('Distance:', dist)
+        print()
