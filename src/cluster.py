5bb488b6f33c0ec3ceb43378fa382fd6b4ab56f1
Furkan Akkurt <furkanakkurt9285@gmail.com>
2023-05-30 15:59:30 +0000

    update script

--- src/cluster.py
+++ src/cluster.py
@@ -1,5 +1,7 @@
 import os, json, argparse
 import numpy as np
+from sklearn.metrics.pairwise import cosine_distances
+from sklearn.cluster import AgglomerativeClustering
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(THIS_FOLDER, '../data')
@@ -8,12 +10,13 @@ parser = argparse.ArgumentParser(description='Cluster words')
 parser.add_argument('--type', type=str, default='kmeans', help='Type of clustering', required=True)
 args = parser.parse_args()
 cluster_type = args.type
-if cluster_type == 'kmeans':
-    from sklearn.cluster import KMeans
-    from sklearn.metrics.pairwise import cosine_distances
+cluster_path = ''
+min_dist = 0
+min_max_dist = 0
+max_dist = 1
+if cluster_type == 'n_clusters':
     cluster_path = os.path.join(data_path, 'cluster_distances.json')
-elif cluster_type == 'agglo':
-    from sklearn.cluster import AgglomerativeClustering
+elif cluster_type == 'distance':
     cluster_path = os.path.join(data_path, 'cluster_counts.json')
     min_dist_path = os.path.join(data_path, 'min-dist.json')
     with open(min_dist_path, 'r') as f:
@@ -49,9 +52,15 @@ for word in words:
     if word in cluster_d or not data[word] or entry_d[word] < 1:
         continue
     print('Word:', word)
-    if cluster_type == 'kmeans':
-        kmeans = KMeans(n_clusters=entry_d[word], random_state=0, n_init="auto").fit(data[word])
-        distances = cosine_distances(kmeans.cluster_centers_)
+    if cluster_type == 'n_clusters':
+        arr = np.array(data[word])
+        agglo = AgglomerativeClustering(n_clusters=entry_d[word], metric='cosine', linkage='average', compute_distances=True).fit(arr)
+        labels = agglo.labels_
+        d = {i: np.array([]) for i in range(agglo.n_clusters_)}
+        for i in set(labels):
+            d[i] = arr[labels == i]
+        center_d = {i: np.mean(d[i], axis=0) for i in d}
+        distances = cosine_distances(np.array(list(center_d.values())))
         flat_arr = np.triu(distances).flatten()
         flat_arr = np.delete(flat_arr, np.where(flat_arr == 0))
         if len(flat_arr) < 1:
@@ -63,9 +72,9 @@ for word in words:
         mean_distance = np.mean(flat_arr)
         print('Mean distance:', mean_distance)
         cluster_d[word] = {'min': min_distance, 'max': max_distance, 'mean': mean_distance}
-    elif cluster_type == 'agglo':
-        agglo = AgglomerativeClustering(distance_threshold=min_max_dist, n_clusters=None, affinity='cosine', linkage='average').fit(data[word])
-        cluster_count = len(set(agglo.labels_))
+    elif cluster_type == 'distance':
+        agglo = AgglomerativeClustering(distance_threshold=min_dist, n_clusters=None, metric='cosine', linkage='average').fit(data[word])
+        cluster_count = agglo.n_clusters_
         print('Cluster count:', cluster_count)
         cluster_d[word] = cluster_count
     with open(cluster_path, 'w') as f:
